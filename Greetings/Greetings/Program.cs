bd5f88e10252ede8c004e798cf8ffb2e6e4ba268
User Kashirin <sanya.kashirin1969@bk.ru>
2025-10-31 11:44:48 +0300

    упражнение 2

---
+++ Greetings/Greetings/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Greetings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            Console.WriteLine("Please enter your name");
+            myName = Console.ReadLine();
+            Console.WriteLine("Hello {0}", myName);
+            Console.ReadLine();
+        }
+    }
+}
