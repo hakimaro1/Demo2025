fc7cf53cae97a76abf454904f49b719f450cb3f5
hakimaro1 <sanya.kashirin1969@bk.ru>
2025-10-31 16:27:33 +0300

    Лабораторная работа 2 упражнения 1 и 2

---
+++ Lab03/Lab03/STARTER/BANKACCOUNT/BankAccount/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+public enum AccountType { Checking, Deposit }
+
+public class Program
+{
+    public static void Main()
+    {
+        AccountType goldAccount;
+        AccountType platinumAccount;
+
+        goldAccount = AccountType.Checking;
+        platinumAccount = AccountType.Deposit;
+
+        Console.WriteLine("The Customer Account Type is {0}", goldAccount);
+        Console.WriteLine("The Customer Account Type is {0}", platinumAccount);
+    }
+}
