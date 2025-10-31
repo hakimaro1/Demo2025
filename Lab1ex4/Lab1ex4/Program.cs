2d2fe95ee15210f70ebd97154a27ed38b014f7bb
hakimaro1 <sanya.kashirin1969@bk.ru>
2025-10-31 17:33:30 +0300

    Лабораторная 1 задание 4 (не обязательное

---
+++ Lab1ex4/Lab1ex4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Divider
+{
+    class DivideIt
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first integer");
+            int i = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second integer");
+            int j = Int32.Parse(Console.ReadLine());
+
+            int k = i / j;
+            int m = i * j;
+
+            Console.WriteLine($"The result of dividing {i} by {j} is {k}");
+            Console.WriteLine($"The result of multiplying {i} by {j} is {m}");
+        }
+    }
+}
