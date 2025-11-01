907b2abdcdd9a0d3e79e132031abb322a1715045
User Kashirin <sanya.kashirin1969@bk.ru>
2025-11-01 11:38:02 +0300

    Лабораторная 2 задание 3 контрольное

--- Distance/Distance/Program.cs
+++ Distance/Distance/Program.cs
@@ -1,63 +1,34 @@
 ﻿using System;
 
-struct Distance
-{
-    public int feet;
-    public int inches;
-
-    public Distance(int feet, int inches)
-    {
-        this.feet = feet;
-        this.inches = inches;
-        Normalize();
-    }
-
-    private void Normalize()
-    {
-        if (inches >= 12)
-        {
-            feet += inches / 12;
-            inches = inches % 12;
-        }
-    }
-
-    public override string ToString()
-    {
-        return $"{feet}'- {inches}\"";
-    }
-}
-
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Введите значения для первого расстояния:");
+        // Ввод первого расстояния
+        Console.WriteLine("Введите первое расстояние:");
         Console.Write("Футы: ");
         int feet1 = int.Parse(Console.ReadLine());
         Console.Write("Дюймы: ");
         int inches1 = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("\nВведите значения для второго расстояния:");
+        // Ввод второго расстояния
+        Console.WriteLine("\nВведите второе расстояние:");
         Console.Write("Футы: ");
         int feet2 = int.Parse(Console.ReadLine());
         Console.Write("Дюймы: ");
         int inches2 = int.Parse(Console.ReadLine());
 
-        Distance distance1 = new Distance(feet1, inches1);
-        Distance distance2 = new Distance(feet2, inches2);
-        Distance distance3;
+        // Переводим всё в дюймы, складываем, затем обратно в футы и дюймы
+        int totalInches = (feet1 * 12 + inches1) + (feet2 * 12 + inches2);
+        int sumFeet = totalInches / 12;
+        int sumInches = totalInches % 12;
 
-        Distance Z;
-        Z.inches = distance1.feet * 12 + distance1.inches + distance2.feet * 12 + distance2.inches;
-        Z.feet = (int)(Z.inches / 12);
-        Z.inches = Z.inches % 12;
-        distance3 = Z;
-
+        // Выводим результаты
         Console.WriteLine("\nРезультаты:");
-        Console.WriteLine($"Первое расстояние: {distance1}");
-        Console.WriteLine($"Второе расстояние: {distance2}");
-        Console.WriteLine($"Сумма: {distance3}");
+        Console.WriteLine($"Первое расстояние: {feet1}'–{inches1}\"");
+        Console.WriteLine($"Второе расстояние: {feet2}'–{inches2}\"");
+        Console.WriteLine($"Сумма: {sumFeet}'–{sumInches}\"");
 
-        Console.ReadKey();
+        Console.ReadKey(); // Чтобы программа не закрылась сразу
     }
 }
